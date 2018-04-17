c912de15253a09bb0f83a67a3786b36cf3127587
Jasper <jaspercheng.is@gmail.com>
2018-04-17 12:35:46 -0700

    Complete modal for info

--- main.js
+++ main.js
@@ -15,21 +15,59 @@ const apparelCombo = function(){
 // Grab all values and delete all "selected: null"
 }
 
-const ensembleSet = function(apparel_combo){
+const ensembleBreak = function(apparel_combo){
 // Check for belt, watch, shoes. If any selected, make sure same color all
+// True means rule is broken
+  return true
 }
 
-const maxNonColor = function(apparel_combo){
+const tooManyNonMono = function(apparel_combo){
   // Ensure max non-monochrome <= 2
+  // True means rule is broken
+  return true
 }
 
-const noOnesies = function(apparel_combo){
+const onesies = function(apparel_combo){
   // Ensure no onesies
+  // True means rule is broken
+  return true
 }
 
-const throwResponse = function(){
+const throwResponse = function(apparelCombo){
   let errors = []
+  if (ensembleBreak(apparelCombo)) {
+    errors.push('Ensemble Pieces(belt, shoes, watch) must be the same color')
+  }
+  if (tooManyNonMono(apparelCombo)) {
+    errors.push('Too many non-monochromatic colors')
+  }
+  if (onsies(apparelCombo)) {
+    errors.push('Must have at least one different color(no onesies!)')
+  }
 
+  return errors
+}
 
+// Get the modal
+var modal = document.getElementById('style-modal');
+console.log(modal);
 
+// Get the button that opens the modal
+var btn = document.getElementById("style-button");
+console.log(btn);
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
