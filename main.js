b3c36144a3b42899a896c1f8b057e044d5032fd8
Jasper <jaspercheng.is@gmail.com>
2018-04-17 11:53:19 -0700

    Add function scaffolding

---
+++ main.js
@@ -0,0 +1,35 @@
+class Hat {
+  constructor(){
+    this.color = null;
+    this.selected = null;
+  }
+
+
+}
+
+const changeProp = function(apparel){
+// Dynamically change value of class' this.color to select prop value
+}
+
+const apparelCombo = function(){
+// Grab all values and delete all "selected: null"
+}
+
+const ensembleSet = function(apparel_combo){
+// Check for belt, watch, shoes. If any selected, make sure same color all
+}
+
+const maxNonColor = function(apparel_combo){
+  // Ensure max non-monochrome <= 2
+}
+
+const noOnesies = function(apparel_combo){
+  // Ensure no onesies
+}
+
+const throwResponse = function(){
+  let errors = []
+
+
+
+}
